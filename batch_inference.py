fd8c6a9c55e38113ede125d36704719a559d3de9
Guanming Wang <84255909+william-Dic@users.noreply.github.com>
2025-03-06 18:31:39 +0000

    Create batch_inference.py

---
+++ batch_inference.py
@@ -0,0 +1,53 @@
+import torch
+from transformers import AutoModelForCausalLM
+
+from deepseek_vl2.models import DeepseekVLV2Processor, DeepseekVLV2ForCausalLM
+from deepseek_vl2.utils.io import load_pil_images
+
+
+model_path = "deepseek-ai/deepseek-vl2-tiny"
+vl_chat_processor: DeepseekVLV2Processor = DeepseekVLV2Processor.from_pretrained(model_path)
+tokenizer = vl_chat_processor.tokenizer
+
+vl_gpt: DeepseekVLV2ForCausalLM = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True)
+vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
+
+conversation = [
+    {
+        "role": "<|User|>",
+        "content": "<image>\n<|ref|>Please extract all the information from the given image<|/ref|>.",
+        "images": ["./images/visual_grounding_1.jpeg"],  # TODO NEED TO BE CHANGED
+    },
+    {"role": "<|Assistant|>", "content": ""},
+]
+
+# Load images and prepare for inputs
+pil_images = load_pil_images(conversation)
+prepare_inputs = vl_chat_processor(
+    conversations=conversation,
+    images=pil_images,
+    force_batchify=True,
+    system_prompt=""
+).to(vl_gpt.device)
+
+# Run image encoder to get the image embeddings
+inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
+
+# Run the model to get the response
+outputs = vl_gpt.language.generate(
+    inputs_embeds=inputs_embeds,
+    attention_mask=prepare_inputs.attention_mask,
+    pad_token_id=tokenizer.eos_token_id,
+    bos_token_id=tokenizer.bos_token_id,
+    eos_token_id=tokenizer.eos_token_id,
+    max_new_tokens=512,
+    do_sample=False,
+    use_cache=True
+)
+
+answer = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=False)
+
+with open("image2txt.txt", "w", encoding="utf-8") as file:
+    file.write(answer)
+
+print(f"Extracted information saved to image2txt.txt")
